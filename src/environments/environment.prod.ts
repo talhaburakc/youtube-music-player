8dc7b44fad8eeced13518c87f283b554887a23e3
talha <talhaburakcuhadar@gmail.com>
2019-09-18 16:21:01 +0300

    added env variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_KEY: '(PUT YOUR YOUTUBE API KEY HERE)',
+  API_ENDPOINT:'https://youtube-music-player-server.herokuapp.com',
+  GOOGLE_AUTH_KEY: '(NOT NEEDED)'
+};
